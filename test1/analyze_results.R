812ee9cf1bad7f651016c457b97071af87c7d0f5
Shrey Shahi <shrey.shahi@gmail.com>
2016-12-12 23:13:34 -0800

    Added R analysis code for plotting

--- test1/analyze_results.R
+++ test1/analyze_results.R
@@ -1,3 +1,12 @@
+library("ggplot2")
+library("dplyr")
+
 training_data = read.csv("./results/training_data.csv")
 test_data = read.csv("./results/test_data.csv")
 
+
+# select best models
+best_models = training_data %>%
+  group_by(depth, lr) %>%
+  slice(which.min(error))
+print(best_models)
